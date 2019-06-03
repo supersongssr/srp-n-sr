1a736841aaae672e9d8265b1c96178d2a6c2bbf3
supersongssr <34210429+supersongssr@users.noreply.github.com>
2019-06-03 16:41:51 +0800

    443端口重定向

--- initcfg.sh
+++ initcfg.sh
@@ -2,7 +2,7 @@
 
 chmod +x *.sh
 chmod +x shadowsocks/*.sh
-cp -n apiconfig.py userapiconfig.py
-cp -n config.json user-config.json
-cp -n mysql.json usermysql.json
+#cp -n apiconfig.py userapiconfig.py
+#cp -n config.json user-config.json
+#cp -n mysql.json usermysql.json
 
